a27982ac8c9913735784f5f5f26ce8bf9108de60
ANIKA MAHFUZA <panelme902@gmail.com>
2026-01-27 01:42:22 +0600

    extra files del

--- Hazy-main/Hazy-main/theme.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    const script = document.createElement("SCRIPT");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute(
-      "src",
-      "https://cdn.jsdelivr.net/gh/astromations/Hazy/hazy.js"
-    );
-    document.head.appendChild(script);
-  })();
