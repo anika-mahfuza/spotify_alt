c5a12275555d625a1642a504517bb1f1de717d77
anika-mahfuza <user@example.com>
2026-01-27 16:36:53 +0600

    Align volume controls to the bottom-right on mobile

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -495,7 +495,7 @@ export function Player({ currentTrack, nextTrack, onNext, onPrev, backendUrl, is
                 </div>
 
                 {/* Right Controls (Volume etc) - Bottom row on mobile */}
-                <div className="flex items-center justify-start md:justify-end w-full md:w-[30%] min-w-[140px] gap-2 px-4 md:px-0 pb-2 md:pb-0">
+                <div className="flex items-center justify-end w-full md:w-[30%] min-w-[140px] gap-2 px-4 md:px-0 pb-2 md:pb-0">
                     <button
                         onClick={toggleMute}
                         className="transition-all text-white/70 hover:text-white"
