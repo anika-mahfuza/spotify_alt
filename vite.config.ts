679c3b50bd1586dc6008dc132c6422f7ef2995f5
ANIKA MAHFUZA <panelme902@gmail.com>
2026-04-11 14:04:12 +0600

    feat: add vite-plugin-sitemap for automatic sitemap generation on every build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://spotify-alt.pages.dev',
+      dynamicRoutes: [
+        '/',
+        '/home',
+        '/import',
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
